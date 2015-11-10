6259daac41f3515c0cafef82bb2c3bf27b2eaab0
monstertaco <anderson.ajmc@gmail.com>
2015-11-09 16:00:45 -0800

    added question 6

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
  var playerName = prompt("What is your name?");
-        console.log(playerName)
-		function runQuestionOne() {
+        console.log(playerName);
+    	function runQuestionOne() {
 			var guess1 = prompt("Have I been to Denmark?").toLowerCase();
-			console.log(guess1)
+			console.log(guess1);
 			if(guess1 == "yes" || guess1 == "y") {
 				alert("Nice job " + playerName + " you got it! I spent a semester in Denmark in 2010!");
 			}
 			else {
 				alert("You're wrong " + playerName + " I studied in Denmark in 2010.");
-			runQuestionOne()
+			runQuestionOne();
 			}
-		};
-		runQuestionOne()
+		}
+		runQuestionOne();
 		function runQuestionTwo() {
 			var guess2 = prompt("Do I have a pet fish?").toLowerCase();
-			console.log(guess2)
+			console.log(guess2);
 			if(guess2 == "no" || guess2 == "n") {
-			alert("Nice job " + playerName + " you got it! I don't have any pets at the moment.")
+			alert("Nice job " + playerName + " you got it! I don't have any pets at the moment.");
 			}
 			else {
 			alert("You're wrong " + playerName + " I don't have any pets right now.");
-			runQuestionTwo()
+			runQuestionTwo();
 			}
 		}
-        runQuestionTwo()
+        runQuestionTwo();
 		function runQuestionThree() {
 			var guess3 = prompt("What state am I from?").toLowerCase();
-			console.log(guess3)
+			console.log(guess3);
 			switch(guess3) {
 				case'california':
 					alert("Nice job " + playerName + " you got it! I grew up in Southern California.");
@@ -45,40 +45,63 @@
 				default:
 					alert("HINT: It's a state on the West Coast.");
 					runQuestionThree();
-			};
-		};
-		runQuestionThree()
+			}
+		}
+		runQuestionThree();
 		function runQuestionFour() {
 			var guess4 = prompt("How old am I?").toLowerCase();
-			console.log(guess4)
-			console.log(parseInt(guess4))
+			console.log(guess4);
+			console.log(parseInt(guess4));
 			if (guess4 == 26 || guess4 == "twenty six" || guess4 == "twenty-six" || guess4 == "twentysix") {
-				alert("You got it! I'm 26 years old!")
+				alert("You got it! I'm 26 years old!");
 				var integerCheck = parseInt(guess4);
 				console.log(integerCheck);
 			}
-			else if (integerCheck !== true) {
-				alert("Please enter a number!")
-				runQuestionFour();
-			}
 			else if (guess4 < 26) {
-				alert("I'm older than that!")
+				alert("I'm older than that!");
 				runQuestionFour();
 			}	
 			else if (guess4 > 26) {
-				alert("I'm younger than that!")
+				alert("I'm younger than that!");
 				runQuestionFour();
-			};
-		};
+			}
+			else if (integerCheck !== true) {
+				alert("Please enter a number!");
+				runQuestionFour();
+			}
+		}
 		runQuestionFour();
 		function runQuestionFive() {
 			var guess5 = prompt("Do I love tacos?").toLowerCase();
 			if (guess5 == "yes" || guess5 == "y") {
-				alert("Yes! Tacos are amazing!")
+				alert("Yes! Tacos are amazing!");
 			}
 			else {
-				alert("Wrong! Try again.")
-				runQuestionFive()
-			};
-		};
-		runQuestionFive()
+				alert("Wrong! Try again.");
+				runQuestionFive();
+			}
+		}
+		runQuestionFive();
+		    function runQuestionSix() {
+			var guess6 = prompt("How many US states have I been to?").toLowerCase();
+			console.log(guess6);
+			console.log(parseInt(guess6));
+			if (guess6 == 44 || guess6 == "forty four" || guess6 == "forty-four" || guess6 == "fortyfour") {
+				alert("You got it! I've been to 44 states!");
+				var integerCheckTwo = parseInt(guess6);
+				console.log(integerCheckTwo);
+			}
+			else if (guess6 < 44) {
+				alert("I've been to more states than that!");
+				runQuestionSix();
+			}	
+			else if (guess6 > 44) {
+				alert("I haven't been to that many states!");
+				runQuestionSix();
+			}
+            else if (integerCheckTwo !== true) {
+    			alert("Please enter a number!");
+				runQuestionSix();
+            }
+		}
+		runQuestionSix();
